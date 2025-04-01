2a247b9fe98b6caff9b26bd3aa6a1a6926e363a7
TianMing <3042568013@qq.com>
2025-04-01 21:02:20 +0800

    最短路 最小生成树 真值表

--- dm/src/pages/function1.vue
+++ dm/src/pages/function1.vue
@@ -1,624 +1,158 @@
-<script>
-import { onMounted ,ref} from 'vue';
-import axios from '@/utlis/axios';
-export default {
-  setup() {
-    const vr = ref(100)
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from '@/utlis/axios' // 你自定义的 axios 封装
 
-    onMounted(() => {
-      const enter = document.querySelector(".enter")
+// 用于存放后端返回的表头与表数据
+const tableHeaders = ref([]) // ["p", "q", "p→q", ...]
+const tableData = ref([])    // 二维数组 [[0,0,1], [0,1,1], [1,0,0], [1,1,1], ...]
 
-    const submit = document.querySelector('.submit')
+onMounted(() => {
+  const enter = document.querySelector('.enter')
+  const submit = document.querySelector('.submit')
 
-     //制作符号按钮功能
-     for(let i=2;i<=7;i++){
-      const tipbutton = document.querySelector(`.tipbut:nth-child(${i})`)
-      
-        tipbutton.addEventListener('click',() =>{
-           // 获取当前光标位置
-          const cursorPosition = enter.selectionStart;
-          // 获取光标前后的文本
-          const startText = enter.value.substring(0, cursorPosition);
-          const endText = enter.value.substring(cursorPosition);
-          const textToInsert=tipbutton.value
-          enter.value = startText + textToInsert + endText;
-          enter.addEventListener('blur',()=>{
-            enter.focus();
-          })
-          //设置光标位置
-          enter.setSelectionRange(cursorPosition + textToInsert.length, cursorPosition + textToInsert.length);
-          // enter.value = enter.value + `${tipbutton.value}`
-        })
-    }
+  // 1）插入符号按钮
+  // 这里假设与前面的写法一致：页面上有若干 .tipbut
+  for (let i = 2; i <= 7; i++) {
+    const tipButton = document.querySelector(`.tipbut:nth-child(${i})`)
+    if (!tipButton) continue
+    tipButton.addEventListener('click', () => {
+      const cursorPosition = enter.selectionStart
+      const startText = enter.value.substring(0, cursorPosition)
+      const endText = enter.value.substring(cursorPosition)
+      const textToInsert = tipButton.value
+      enter.value = startText + textToInsert + endText
 
-    const TwoBT = document.querySelector('.two')
-    const ThreeBT = document.querySelector('.three')
-    const FourBT = document.querySelector('.four')
-    const TwoForm = document.querySelector('.Twobianyuan')
-    const ThreeForm = document.querySelector('.Tbianyuan')
-    const FourForm = document.querySelector('.Fbianyuan')
-    TwoBT.addEventListener('click',()=>{
-      // TwoForm.style.display = 'block'
-      // ThreeForm.style.display = 'none'
-      // FourForm.style.display = 'none'
-      TwoForm.classList.remove('hide','show')
-      ThreeForm.classList.remove('hide','show')
-      FourForm.classList.remove('hide','show')
-
-      TwoForm.classList.add('show')
-      ThreeForm.classList.add('hide')
-      FourForm.classList.add('hide')
+      // 设置光标位置
+      enter.setSelectionRange(
+        cursorPosition + textToInsert.length,
+        cursorPosition + textToInsert.length
+      )
+      enter.focus()
     })
-    ThreeBT.addEventListener('click',()=>{
-      TwoForm.classList.remove('hide','show')
-      ThreeForm.classList.remove('hide','show')
-      FourForm.classList.remove('hide','show')
-
-      TwoForm.classList.add('hide')
-      ThreeForm.classList.add('show')
-      FourForm.classList.add('hide')
-    })
-    FourBT.addEventListener('click',()=>{
-      TwoForm.classList.remove('hide','show')
-      ThreeForm.classList.remove('hide','show')
-      FourForm.classList.remove('hide','show')
-
-      TwoForm.classList.add('hide')
-      ThreeForm.classList.add('hide')
-      FourForm.classList.add('show')
-    })
-
-    
-
-    //点击提交数据传送至后端并返回结果
-    submit.addEventListener('click',function(){
-      // const character = document.querySelector('.Tbianyuan .answer .answer_char')
-      // console.log(enter.value)
-      // character.innerHTML = enter.value
-
-      axios({
-        url:`/zhenzhi/${enter.value}`,
-        method:'GET',
-
-      }).then(result => {
-
-        console.log(result)
-
-        const arr_answer = result.data.data.split(',')
-        
-    for(let i=2;i<=arr_answer.length+1;i++){
-      if(arr_answer.length<=4){
-        const answer_ul = document.querySelector(`.Twobianyuan .answer .answer_ul li:nth-child(${i})`)
-        answer_ul.innerHTML = arr_answer[i-2]
-        const character = document.querySelector('.Twobianyuan .answer .answer_char')
-        character.innerHTML = enter.value
-
-      }else if(arr_answer.length<=8){
-        const answer_ul = document.querySelector(`.Tbianyuan .answer .answer_ul li:nth-child(${i})`)
-        answer_ul.innerHTML = arr_answer[i-2]
-        const character = document.querySelector('.Tbianyuan .answer .answer_char')
-        character.innerHTML = enter.value
-      }else{
-        const answer_ul = document.querySelector(`.Fbianyuan .answer .answer_ul li:nth-child(${i})`)
-        answer_ul.innerHTML = arr_answer[i-2]
-        const character = document.querySelector('.Fbianyuan .answer .answer_char')
-        character.innerHTML = enter.value
-      }
-    }
-
-
-      })
-    })
-    //测试渲染
-    
-    
-
-    // const temp ='1,1,1,0,1,0,1,0'
-    // console.log(temp)
-    // arr_answer = temp.split(',')
-    // console.log(arr_answer.length)
-    // for(let i=2;i<=arr_answer.length+1;i++){
-    //   if(arr_answer.length<=4){
-    //     const answer_ul = document.querySelector(`.Twobianyuan .answer .answer_ul li:nth-child(${i})`)
-    //     answer_ul.innerHTML = arr_answer[i-2]
-    //   }else if(arr_answer.length<=8){
-    //     const answer_ul = document.querySelector(`.Tbianyuan .answer .answer_ul li:nth-child(${i})`)
-    //     answer_ul.innerHTML = arr_answer[i-2]
-    //   }else{
-    //     const answer_ul = document.querySelector(`.Fbianyuan .answer .answer_ul li:nth-child(${i})`)
-    //     answer_ul.innerHTML = arr_answer[i-2]
-    //   }
-    // }
-
-
-    });
-    return { 
-        vr
-    }
   }
-}
 
+  // 2）提交按钮：向后端请求真值表
+  submit.addEventListener('click', () => {
+    const formula = enter.value.trim()
+    if (!formula) return
+    axios.get(`/zhenzhi/${formula}`)
+      .then(res => {
+        const resp = res.data
+        tableHeaders.value = resp.va || []
+        tableData.value = resp.data || []
+        console.log(resp)
+      })
+      .catch(err => {
+        console.error('请求异常:', err)
+      })
+  })
+})
 </script>
 
- 
-
 <template>
-
   <div class="banner">
-
-    <el-card style="min-width: 480px;width:550px" class="leftbanner">
-
+    <!-- 左侧输入区 -->
+    <el-card style="min-width: 580px; width: 550px" class="leftbanner">
       <template #header>
         <div class="card-header title">
-         <span >命题公式真值表</span>
+          <span>命题公式真值表</span>
         </div>
       </template>
 
-
-      <el-button  value="两变原"  class="two" style="width: 80px;border: 2px solid;">两变原</el-button>
-      <el-button  value="三变原"  class="three" style="width: 80px;border: 2px solid;">三变原</el-button>
-      <el-button  value="四变原"  class="four" style="width: 80px;border: 2px solid;">四变原</el-button>
-      <el-card style="min-width: 480px;height: 200px" class="decoration">
-
+      <el-card style="min-width: 430px; height: 200px" class="decoration">
         <div class="tip">请输入命题公式:</div>
-        <el-button type="button" class="tipbut" value="∨" id="tb2">∨</el-button>
- 
-        <el-button type="button" value="∧" class="tipbut" id="tb3">∧</el-button>
 
-        <el-button type="button" value="→" class="tipbut" id="tb4">→</el-button>
- 
-        <el-button type="button" value="﹁" class="tipbut" id="tb5">﹁</el-button>
+        <!-- 逻辑符号按钮 -->
+        <el-button type="button" class="tipbut" value="∨">∨</el-button>
+        <el-button type="button" class="tipbut" value="∧">∧</el-button>
+        <el-button type="button" class="tipbut" value="→">→</el-button>
+        <el-button type="button" class="tipbut" value="﹁">﹁</el-button>
+        <el-button type="button" class="tipbut" value="↔">↔</el-button>
+        <el-button type="button" class="tipbut" value="()">()</el-button>
 
-        <el-button type="button" value="↔" class="tipbut" id="tb6">↔</el-button>
-
-        <el-button type="button" value="()" class="tipbut" id="tb7">()</el-button>
-
-        
-        <input type="text" placeholder='例如：(p∨q)->r∨s' class="enter">
+        <!-- 输入框 + 提交按钮 -->
+        <input
+          type="text"
+          placeholder="例如：(p∨q)->r∨s"
+          class="enter"
+        />
         <el-button type="primary submit" class="submit">提交</el-button>
+      </el-card>
+    </el-card>
 
-       </el-card>
-      
-   </el-card>
-    
-
-    <div class="rightbanner" style="border: 0px solid;background-color: white;">
-      <!-- 四变原 -->
-      <div class="Fbianyuan hide">
-      <div class="enter">
-        <ul>
-          <li class="character">p</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-        </ul>
-        <ul>
-          <li class="character">q</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-        </ul>
-        <ul>
-          <li class="character">r</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-        </ul>
-        <ul>
-          <li class="character">s</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-        </ul>
-      </div>
-      <div class="answer">
-        <ul class="answer_ul">
-          <li class="answer_char"></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-    <!-- 三变原 -->
-    <div class="Tbianyuan hide">
-      <div class="enter">
-        <ul>
-          <li class="character">p</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-        </ul>
-        <ul>
-          <li class="character">q</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>
-        </ul>
-        <ul>
-          <li class="character">r</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-        </ul>
-      </div>
-      <div class="answer">
-        <ul class="answer_ul">
-          <li class="answer_char"></li>
-          <li data-="1"></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-    <!-- 双变元 -->
-    <div class="Twobianyuan show">
-      <div class="enter">
-        <ul>
-          <li class="character">p</li>
-          <li>0</li>
-          <li>0</li>
-          <li>1</li>
-          <li>1</li>  
-        </ul>
-        <ul>
-          <li class="character">q</li>
-          <li>0</li>
-          <li>1</li>
-          <li>0</li>
-          <li>1</li>
-        </ul>
-      </div>
-      <div class="answer">
-        <ul class="answer_ul">
-          <li class="answer_char"></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-
+    <!-- 右侧显示表格 - 外层容器控制滚动和自适应 -->
+    <div class="table-container">
+      <el-table :data="tableData" border style="width: auto" height="500" :header-cell-style="{ textAlign: 'center' , backgroundColor: '#000', color: '#fff'}">
+        <!-- 动态生成列：根据后端返回的表头数组 -->
+        <el-table-column
+          v-for="(header, hIndex) in tableHeaders"
+          :key="hIndex"
+          :label="header"
+          :align="center"
+        >
+          <!-- 单元格的具体展示：row[hIndex] -->
+          <template #default="{ row }">
+            {{ row[hIndex] }}
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
-
 </template>
 
-<!-- <script>
-export default {
-
-}
-</script> -->
-
-
 <style scoped>
-    .clearfix{zoom:1;}
-    .clearfix::after {
-    content: ".";
-    display: block;
-    height: 0;
-    clear: both;
-    visibility: hidden;
-    }
+.banner {
+  display: flex;
+  justify-content: center;
+  min-width: 1240px;
+}
+.leftbanner {
+  width: 600px;
+  margin: auto;
+}
+.decoration {
+  width: 450px;
+  height: 200px;
+  margin-top: 100px;
+  position: relative;
+}
+.tip {
+  font-size: 20px;
+  padding-top: 10px;
+  margin-left: 10px;
+}
+.tipbut {
+  margin-top: 10px;
+  margin-left: 20px;
+}
+.enter {
+  margin-top: 30px;
+  margin-left: 80px;
+  height: 30px;
+} 
+.submit {
+  height: 32px;
+  width: 60px;
+  margin-left: 10px;
+}
 
-    .banner{
-      height: 550px;
-      min-width: 1240px;
-      display: flex;
-      margin: 0 auto;
-    }
-    .banner .leftbanner{
-      height:480px;
-      width: 600px;
-      border: 1px solid;
-      margin-left: 50px;
-      margin-top: 30px;
-      position: relative;
-    }
-    .leftbanner h1{
-      width: 360px;
-      border-bottom: 4px solid black;
-    }
-    .leftbanner .title{
-      font-size: 50px;
-    }
-    .leftbanner .tip{
-      font-size: 20px;
-      padding-top: 10px;
-      margin-left: 10px;
+/* 让表格容器可在内容过宽时滚动，实现“大小自适应” */
+.table-container {
+  margin-left: 20px;      /* 右侧表格和左侧卡片分开一些 */
+  border: 0 solid;
+  overflow-x: auto;      /* 如果表格过宽，则出现横向滚动条 */
+  /* 如果希望固定最大高度，也可加: max-height: 400px; overflow-y: auto; */
+  width: 100%;           /* 让表格容器宽度自适应 */
+  height: 500px;         /* 固定高度 */
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 
+.el-table {
+  width: auto;           /* 让表格宽度自适应 */
+}
+/* .el-table .cell {
+  white-space: nowrap;
+} */
 
-    }
-    .leftbanner .tipbut{
-      height:32px;
-      width: 40px;
-      margin-top: 10px;
-      margin-left: 20px;
-      float: left;
-      display: block;
-    }
-    .leftbanner .enter{
-      margin-top: 30px;
-      margin-left: 80px;
-      height: 30px;
-    }
-    .leftbanner .decoration{
-      width :450px;
-      height:200px;
-      margin-left: 100px;
-      margin-top: 100px;
-      border: 1px solid black;
-      position: absolute;
-      right: 50%;
-      bottom: 10%;
-      transform: translateX(50%);
-    }
-    .leftbanner .submit{
-      height:32px;
-      width: 60px;
-      margin-left: 10px;
-      position: relative;
-
-    }
-
-    .rightbanner{
-      height: 500px;
-      width: 650px;
-      margin-top: 10px;
-      margin-left: 30px;
-      right: 150px;
-    }
-    .show{
-      display: block;
-    }
-    .hide{
-      display: none;
-    }
-    /* 表格按钮 */
-    .banner .leftbanner .two{
-      height:32px;
-      width: 60px;
-      margin-top: 10px;
-      margin-left: 40px;
-      float: left;
-      display: block;
-    }
-    .banner .leftbanner .three{
-      height:32px;
-      width: 60px;
-      margin-top: 10px;
-      margin-left: 40px;
-      float: left;
-      display: block;
-    }
-    .banner .leftbanner .four{
-      height:32px;
-      width: 60px;
-      margin-top: 10px;
-      margin-left: 40px;
-      float: left;
-      display: block;
-    }
-    /* 双变原css */
-
-    .rightbanner .Twobianyuan .enter ul{
-      height: 500px;
-      width: 150px;
-      float: left;
-      list-style: none;
-      margin-left: -1px;
-    }
-    .rightbanner .Twobianyuan .enter li{
-      height: 100px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-    }
-    .rightbanner .Twobianyuan .enter .character{
-      height: 100px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-
-    .rightbanner .Twobianyuan .answer ul{
-      height: 500px;
-      width: 300px;
-      float: left;
-      list-style: none;
-      margin-left: -1px;
-    }
-
-    .rightbanner .Twobianyuan .answer li{
-      height: 100px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-    }
-
-    .rightbanner .Twobianyuan .answer .answer_char{
-      height: 100px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-    /* 三变原CSS */
-
-    .rightbanner .Tbianyuan .enter ul{
-      height: 500px;
-      width: 100px;
-      float: left;
-      list-style: none;
-      margin-left: -1px;
-    }
-    .rightbanner .Tbianyuan .enter li{
-      height: 55px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-    }
-    .rightbanner .Tbianyuan .enter .character{
-      height: 50px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-
-    .rightbanner .Tbianyuan .answer ul{
-      height: 500px;
-      width: 300px;
-      float: left;
-      list-style: none;
-      margin-left: -1px;
-    }
-
-    .rightbanner .Tbianyuan .answer li{
-      height: 55px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-    }
-
-    .rightbanner .Tbianyuan .answer .answer_char{
-      height: 50px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-    /* 四变原 */
-
-    .rightbanner .Fbianyuan .enter ul{
-      height: 500px;
-      width: 74px;
-
-      float: left;
-      list-style: none;
-      margin-left: -1px;
-    }
-    .rightbanner .Fbianyuan .enter li{
-      height: 28px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-    }
-    .rightbanner .Fbianyuan .enter .character{
-      height: 40px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-    .rightbanner .Fbianyuan .answer ul{
-      height: 500px;
-      list-style: none;
-
-    }
-    .rightbanner .Fbianyuan .answer li{
-      height: 28px;
-      border: 1px solid black;
-      margin-top: -1px;
-      text-align: center;
-      line-height: 28px;
-      list-style: none;
-    }
-    .rightbanner .Fbianyuan .answer .answer_char{
-      height: 40px;
-      border: 3px solid black;
-      text-align: center;
-      line-height: 40px;
-    }
-    body{
-      background-color: #ececec42;
-    }
-  </style>
-
+</style>

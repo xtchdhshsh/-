789dadab3c86e337ce122139659ab84d93ecd60c
today_eat_what <15115999+todayeatwhat@user.noreply.gitee.com>
2025-04-11 10:46:51 +0800

    jwt相关更改

--- lisan/src/main/java/comlisanshuxue/lisan/Utils/JWTUtils.java
+++ lisan/src/main/java/comlisanshuxue/lisan/Utils/JWTUtils.java
@@ -3,27 +3,33 @@ package comlisanshuxue.lisan.Utils;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
 
 import static javax.crypto.Cipher.SECRET_KEY;
 
 public class JWTUtils {
 
-    //JWT加密key
-    private static final String SECRET_KEY = "SZTU@2025.$#";
+    // 生成符合 HS512 要求的 512 位密钥（推荐）
+    private static final SecretKey KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     //获取JWT
-    public static String getToken(String username,Integer IsTeacher) {
+    public static String getToken(String username,Integer IsTeacher)  {
         return  Jwts.builder()
                 .claim("username", username)
                 .claim("IsTeacher", IsTeacher)
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .signWith(KEY,Jwts.SIG.HS512)
                 .compact();
     }
 
     // 验证token  合法性
     private static boolean validateToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(authToken);
+            Jwts.parser()
+                    .verifyWith(KEY)
+                    .build()
+                    .parseSignedClaims(authToken);
             return true;
         } catch (Exception e) {
             System.out.println("无效的token：" + authToken);
